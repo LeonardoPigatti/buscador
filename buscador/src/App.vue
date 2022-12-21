fb8a641d8ef267dc4ee0271f61455a408b4e0fb2
LeonardoPigatti <87331397+LeonardoPigatti@users.noreply.github.com>
2022-12-21 16:50:10 -0300

    sixth commit

--- buscador/src/App.vue
+++ buscador/src/App.vue
@@ -9,9 +9,12 @@
         </p>
         <input @keyup= "getUser" id="search" type="text" class="form-control" required/>
       </div>
-      <div class="row" v-if="user.length !==0">
+      <div class="row mt-3" v-if="user.length !==0">
         <div class="col-md-4">
           <Profile :user="user"/>
+        </div>
+        <div class="colmd-8">
+          <Repo v-for="repo in repos" :key="repo" :repo="repo"/>
         </div>
       </div>
     </div>
@@ -20,7 +23,9 @@
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Repo from "./components/Repo.vue";
 import Profile from "./components/Profile.vue";
+
 import axios from 'axios';
 
 
@@ -41,16 +46,20 @@ export default {
   },
   components:{
     Navbar,
-    Profile
+    Profile,
+    Repo
   },
   methods:{
     getUser(e){
       const user= e.target.value;
-      const{url,client_id,client_secret } = this.github;
+      const{url,client_id,client_secret,count,sort } = this.github;
 
       axios.get(
         `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
         ).then(({data}) => this.user =data);
+
+
+        axios.get(`${url}/${user}/repos?per_page=${count}&sort=${sort}$client_id=${client_id}&client_secret=${client_secret}`).then(({data}) => this.repos = data)
     }
   }
  
--- buscador/src/components/Profile.vue
+++ buscador/src/components/Profile.vue
@@ -3,11 +3,11 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="" />
+                    <img class="card-img-top" :src="user.avatar_url" />
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Repositorios:<span class="badge badge-success">{{user.public_repos}}</span></li>
                         <li class="list-group-item">Seguidores:<span class="badge">{{ user.followers }}</span></li>
-                        <li class="list-group-item">Seguindo:<span class="badge">{{ user.following }}</span></li>
+                        <li class="list-group-item">Seguindo:<span class="badge badge-info">{{ user.following }}</span></li>
                     </ul>
                     <div class="card-body">
                         <a :href="user.html_url" class="btn btn-success">Olhar Perfil</a>
