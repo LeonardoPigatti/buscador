b683a8988802945510137cce31b9eecdeb986603
LeonardoPigatti <87331397+LeonardoPigatti@users.noreply.github.com>
2022-12-21 17:03:03 -0300

    seventh commit

--- buscador/src/components/Repo.vue
+++ buscador/src/components/Repo.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="card card-body-mb-2">
+    <div class="card card-body mb-2">
         <div class="row">
-            <div class="col-md-6"><a href="" target="_black"></a></div>
+            <div class="col-md-6"><a :href="repo.html_url" target="_blank">{{ repo.name }}</a></div>
             <div class="col-md-6">
-                <span class="badge badge-primary"></span>
-                <span class="badge badge-primary"></span>
-                <span class="badge badge-primary"></span>
+                <span class="badge badge-primary">Stars: {{ repo.statgazers_count }}</span>
+                <span class="badge badge-secondary">Watch: {{ repo.watchers_count }}</span>
+                <span class="badge badge-warning">Forks: {{ repo.forks_count }}</span>
 
             </div>
         </div>
